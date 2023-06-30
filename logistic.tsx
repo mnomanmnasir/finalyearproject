67068e185236461c0f58c48661356ad08ad1ff9a
Noman Muhammad Nasir <102538287+mnomanmnasir@users.noreply.github.com>
2023-06-30 11:16:47 -0700

    working on logistic page

---
+++ logistic.tsx
@@ -0,0 +1,65 @@
+import Head from "next/head";
+import Sidebar from "../../components/Sidebar";
+import { BsGrid3X3GapFill, BsFillPencilFill, BsTrash3Fill } from 'react-icons/bs';
+
+const Logistic = () => {
+    return (
+        <>
+            <Head>
+                <title>
+                    Logistic Page
+                </title>
+            </Head>
+
+
+            <div className="container-fluid">
+                <div className="row flex-nowrap">
+                    <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+                        <div className="d-flex flex-column px-0 pt-0 text-white min-vh-100">
+
+                            <Sidebar />
+                        </div>
+                    </div>
+                    <div className="col py-3">
+                        <h1 style={{ fontWeight: 'bold', fontSize: '2rem', margin: '1%' }}>
+                            Logistics
+                        </h1>
+                        <table className="table table-dark table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Group</th>
+                                    <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Ahad</td>
+                                    <td>Mansoori</td>
+                                    <td className="table-dark">
+
+                                        <button>
+                                            <BsFillPencilFill />
+                                        </button>
+
+                                        <button>
+                                            <BsTrash3Fill />
+                                        </button>
+                                         </td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Logistic;
+
+
+
+
+
