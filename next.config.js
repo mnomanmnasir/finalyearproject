ed32317a4b1b31e9f7d33d56edf60efbb50736a0
Noman Muhammad Nasir <102538287+mnomanmnasir@users.noreply.github.com>
2023-07-01 02:29:55 -0700

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  basePath: "/finalyearproject",
+  experimental: { images: { unoptimized: true } }, basePath: "/finalyearproject", output: 'export',
+  images: {unoptimized: true},experimental: {images: {unoptimized: true}},basePath: "/finalyearproject",output: 'export'
 }
 
 module.exports = nextConfig
