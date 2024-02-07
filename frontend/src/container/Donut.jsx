41e4159e3fe621ec979ca20d9da6d62b5e8007c2
Noman Muhammad Nasir <102538287+mnomanmnasir@users.noreply.github.com>
2024-02-07 12:37:24 -0800

    updated

--- frontend/src/container/Donut.jsx
+++ frontend/src/container/Donut.jsx
@@ -1,36 +1,50 @@
-import React from 'react';
-import Chart from 'react-apexcharts';
+import React from 'react'
+import Sdata from './SData'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import sliderImg1 from '../assests/sliderImg1.jpg'
+import sliderImg2 from '../assests/sliderImg2.jpg'
+import sliderImg3 from '../assests/sliderImg3.jpg'
+import sliderImg4 from '../assests/sliderImg4.jpg'
+import './Donut.css'
 
-const LineChart = () => {
+const Donut = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    // appendDots: (dots) => {
+    //   return <ul style={{ margin: '0px' }}>{dots}</ul>
+    // }
 
-  const chartOptions = {
-    chart: {
-      type: 'donut',
-    },
-    labels: ['January', 'February', 'March', 'April', 'May'],
-    responsive: [
-      {
-        breakpoint: 480,
-        options: {
-          chart: {
-            width: 200,
-          },
-          legend: {
-            position: 'bottom',
-          },
-        },
-      },
-    ],
-  };
+  }
 
-  const chartSeries = [12, 19, 3, 5, 2];
+
   return (
     <>
-      <div className='row my-2 py-5 m-3 p-5'>
-        <Chart options={chartOptions} series={chartSeries} type="donut" width={400} />
+      <div className="slider-container">
+        <Slider {...settings}>
+          <div className='slide'>
+            <img src={sliderImg1} alt="Slide 1"  />
+          </div>
+          <div className='slide'>
+            <img src={sliderImg2} alt="Slide 2"/>
+            {/* <div className="slider-text-overlay">
+          </div> */}
+          </div>
+          <div className='slide'>
+            <img src={sliderImg3} alt="Slide 3" />
+          </div>
+          <div className='slide'>
+            <img src={sliderImg4} alt="Slide 4" />
+          </div>
+        </Slider>
       </div>
     </>
   )
-
 }
-export default LineChart;
+export default Donut;   
