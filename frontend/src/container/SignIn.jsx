d3faa74ed1f46a2f2a72ea475b6d8658d8910102
Noman Muhammad Nasir <102538287+mnomanmnasir@users.noreply.github.com>
2024-02-11 11:58:50 -0800

    updated

--- frontend/src/container/SignIn.jsx
+++ frontend/src/container/SignIn.jsx
@@ -11,7 +11,7 @@ import Cookies from 'js-cookie';
 import video from '../assests/video.mp4'
 import { useTypewriter, Cursor } from 'react-simple-typewriter'
 import Helmet from './Helmet'
-
+import '../styles/signIn.css'
 
 
 const SignIn = () => {
@@ -26,12 +26,43 @@ const SignIn = () => {
     };
 
 
+    // const wordsList = ['Inventory Management', '\nOrder Management'];
+
+    const wordsList = ['Inventory Management \nOrder Management \nAutomation and Robotics \nEffective Inventory \nIntegration Capabilities \nReal Time Tracking'];
+
+
+    // const formattedWordsList = wordsList.map(word => `- ${word}`);
+
     const [typingEffect] = useTypewriter({
-        words: ['Inventory Management', 'Order Management', 'Automation and Robotics', 'Effective Inventory', 'Integration Capabilities', 'Real Time Tracking'],
+        words: wordsList,
         loop: {},
         typeSpeed: 100,
         deleteSpeed: 40,
     });
+
+
+    // const [typingEffect2] = useTypewriter({
+    //     words: wordsList2,
+    //     loop: {},
+    //     typeSpeed: 100,
+    //     deleteSpeed: 40,
+    // });
+
+
+    // const [typingEffect, setTypingEffect] = useState('');
+    // const [index, setIndex] = useState(0);
+
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         setTypingEffect(wordsList[index]);
+    //         setIndex((prevIndex) => (prevIndex + 1) % wordsList.length);
+    //     }, 2000); // Change the duration as needed
+
+    //     return () => clearInterval(interval);
+    // }, [index, wordsList]);
+
+
+
     const [text, setText] = useState('');
     const [loginInfo, setLoginInfo] = useState({ username: '', password: '' });
     const [passwordShown, setPasswordShown] = useState(false);
@@ -81,89 +112,106 @@ const SignIn = () => {
     };
 
 
+    // const [index, setIndex] = useState(0);
+    // const [word, setWord] = useState('');
+
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         setWord(wordsList[index]);
+    //         setIndex((prevIndex) => (prevIndex + 1) % wordsList.length);
+    //     }, 2000); // Change the duration as needed
+
+    //     return () => clearInterval(interval);
+    // }, [index, wordsList]);
+
+
     return (
         <Helmet title='Sign In'>
-        <Container fluid className="p-0">
-            <Row className="m-0">
-                <Col md={8} className="d-flex align-items-center p-0 mb-0 bg-black position-relative">
-                    {/* <Card> */}
-                    <video
-                        style={{ width: '100%', height: '641px', objectFit: 'cover', opacity: 0.2 }} controls={false} autoPlay // Add autoPlay attribute to start the video automatically
-                        loop
-                        muted>
-                        <source src={video} />
-                    </video>
-                    <div className="typewriter-container text-left  position-absolute top-0 start-0" style={{ position: 'absolute', zIndex: 1, fontFamily: 'Protest Guerrilla, sans-serif', fontSize: '24px', margin: '100px', marginLeft: '100px', fontWeight: 'bold', color: 'white', fontStyle: 'italic' }}>
-                        <h3 style={{fontWeight: 'bold'}}>
-                            Welcome To Warehouse Management System
-                        </h3>
-                        <span>
-                            - {typingEffect}
-                        </span>
-                        <Cursor cursorStyle='|' />
-                    </div>
-                </Col>
-                <Col md={4} className="text-center d-flex align-items-center mb-0">
-                    {/* <Card style={{ width: '700px', marginLeft: '-12px', height: '620px' }} className='p-5'> */}
-                    <Card.Body style={{ width: '700px', marginLeft: '-12px', height: '620px', fontFamily: 'Protest Guerrilla, sans-serif', fontStyle: 'italic', fontWeight: 'bold' }} className='p-5'>
-                        <Card.Title className="mt-5 fs-3">ADMIN LOGIN</Card.Title>
-                        <Form onSubmit={handleSubmit}>
-                            <Form.Group className="mb-3 mt-5">
-                                <Form.Label >USERNAME</Form.Label>
-                                <Form.Control
-                                    className="rounded"
-                                    type="text"
-                                    name="username"
-                                    value={loginInfo.username}
-                                    onChange={handleInputChange}
-                                    required
-                                    icon={BsPersonFill}
-                                />
-                                {/* <BsPersonFill style={{ position: 'absolute', top: '50%', transform: 'translateY(-50%)', left: '10px' }} /> */}
-                            </Form.Group>
-                            <Form.Group className="mb-3 ">
-                                <Form.Label>PASSWORD</Form.Label>
-                                <InputGroup>
+            <Container fluid className="p-0">
+                <Row className="m-0">
+                    <Col md={8} className="d-flex align-items-center p-0 mb-0 bg-black position-relative">
+                        {/* <Card> */}
+                        <video
+                            style={{ width: '100%', height: '641px', objectFit: 'cover', opacity: 0.2 }} controls={false} autoPlay // Add autoPlay attribute to start the video automatically
+                            loop
+                            muted>
+                            <source src={video} />
+                        </video>
+                        <div className="typewriter-container text-left  position-absolute top-0 start-0" style={{ position: 'absolute', zIndex: 1, fontFamily: 'Protest Guerrilla, sans-serif', fontSize: '24px', margin: '100px', marginLeft: '100px', fontWeight: 'bold', color: 'white', fontStyle: 'italic' }}>
+                            <h3 style={{ fontWeight: 'bold' }}>
+                                Welcome To Warehouse Management System
+                            </h3>
+
+                            <ul>
+                                {typingEffect.split('\n').map((word, index) => (
+                                    <li key={index}>
+                                        {index === 0 ? '' : ''} {word} <Cursor/>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </Col>
+                    <Col md={4} className="text-center d-flex align-items-center mb-0">
+                        {/* <Card style={{ width: '700px', marginLeft: '-12px', height: '620px' }} className='p-5'> */}
+                        <Card.Body style={{ width: '700px', marginLeft: '-12px', height: '620px', fontFamily: 'Protest Guerrilla, sans-serif', fontStyle: 'italic', fontWeight: 'bold' }} className='p-5'>
+                            <Card.Title className="mt-5 fs-3">ADMIN LOGIN</Card.Title>
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group className="mb-3 mt-5">
+                                    <Form.Label >USERNAME</Form.Label>
                                     <Form.Control
-                                        type={passwordShown ? "text" : "password"}
-                                        name="password"
-                                        value={loginInfo.password}
+                                        className="rounded"
+                                        type="text"
+                                        name="username"
+                                        value={loginInfo.username}
                                         onChange={handleInputChange}
                                         required
+                                        icon={BsPersonFill}
                                     />
-                                    <InputGroup.Text onClick={togglePasswordVisibility}>
-                                        {passwordShown ? <BsFillEyeSlashFill /> : <BsFillEyeFill />}
-                                    </InputGroup.Text>
-                                </InputGroup>
-                            </Form.Group>
-
-                            <div className='d-flex justify-content-between'>
-                                <Form.Group className="">
-                                    <Form.Check
-                                        type="checkbox"
-                                        label="Remember me"
-                                    />
+                                    {/* <BsPersonFill style={{ position: 'absolute', top: '50%', transform: 'translateY(-50%)', left: '10px' }} /> */}
+                                </Form.Group>
+                                <Form.Group className="mb-3 ">
+                                    <Form.Label>PASSWORD</Form.Label>
+                                    <InputGroup>
+                                        <Form.Control
+                                            type={passwordShown ? "text" : "password"}
+                                            name="password"
+                                            value={loginInfo.password}
+                                            onChange={handleInputChange}
+                                            required
+                                        />
+                                        <InputGroup.Text onClick={togglePasswordVisibility}>
+                                            {passwordShown ? <BsFillEyeSlashFill /> : <BsFillEyeFill />}
+                                        </InputGroup.Text>
+                                    </InputGroup>
                                 </Form.Group>
 
-                                <div className="d-flex justify-content-between align-items-center ">
-                                    <Form.Text className="text-muted">
-                                        Forgot Password?
-                                    </Form.Text>
+                                <div className='d-flex justify-content-between'>
+                                    <Form.Group className="">
+                                        <Form.Check
+                                            type="checkbox"
+                                            label="Remember me"
+                                        />
+                                    </Form.Group>
+
+                                    <div className="d-flex justify-content-between align-items-center ">
+                                        <Form.Text className="text-muted">
+                                            Forgot Password?
+                                        </Form.Text>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <Button variant="dark" type="submit" className="mt-5 align-center w-25 rounded-pill text-center"
-                            >
-                                Login
-                            </Button>
-                        </Form>
-                    </Card.Body>
-                    {/* </Card> */}
-                    <ToastContainer position="top-right" autoClose={5000} />
-                </Col>
-            </Row>
+                                <Button variant="dark" type="submit" className="mt-5 align-center w-25 rounded-pill text-center"
+                                >
+                                    Login
+                                </Button>
+                            </Form>
+                        </Card.Body>
+                        {/* </Card> */}
+                        <ToastContainer position="top-right" autoClose={5000} />
+                    </Col>
+                </Row>
 
-        </Container >
+            </Container >
         </Helmet>
     );
 };
